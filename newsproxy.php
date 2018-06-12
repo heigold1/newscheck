c6aecce2c4b7ed6a73d167b63b642c0a94842ddf
Brent Heigold <brent@heigoldinvestments.com>
2018-06-12 07:15:51 -0700

    New marketwatch website, updating links

--- newsproxy.php
+++ newsproxy.php
@@ -1,44 +1,41 @@
 <?php 
 
+/* 
+oauth_consumer_key: 874c996f1f6ecaa46c65abb115da9912
+consumer_secret: 886529f1c9d06729e97b6f511a89b4df
+*/
+
+include 'config.php';
+
 require_once("simple_html_dom.php"); 
-error_reporting(0); 
+error_reporting(0);
 
-$filename = "test.txt";
-$file = fopen( $filename, "w" );
-if( $file == false )
-{
-   echo ( "Error in opening new file" );
-   exit();
-}
 
-// convert the returning json arrays into UTF-8.
 
-function utf8ize($d) {
-    if (is_array($d)) {
-        foreach ($d as $k => $v) {
-            $d[$k] = utf8ize($v);
-        }
-    } else if (is_string ($d)) {
-        return utf8_encode($d);
-    }
-    return $d;
-}
-
+// header('Content-type: text/html');
 $symbol=$_GET['symbol'];
 $host_name=$_GET['host_name'];
 $which_website=$_GET['which_website'];
 $stockOrFund=$_GET['stockOrFund']; 
+$google_keyword_string = $_GET['google_keyword_string'];
 
-fwrite($file, $symbol . " is a " . $stockOrFund);
+// $html_return=file_get_contents($url);
+
+// fwrite( $file, "stockOrFund is " . $stockOrFund);
+
+// fwrite( $file, "which_website is  " . $which_website);
 
 fopen("cookies.txt", "w");
 
-function grabHTML($host_name, $url)
-{
-$ch = curl_init();
 
+
+
+function grabHTML($function_host_name, $url)
+{
+
+$ch = curl_init();
 $header=array('GET /1575051 HTTP/1.1',
-    "Host: $host_name",
+    "Host: $function_host_name",
     'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language:en-US,en;q=0.8',
     'Cache-Control:max-age=0',
@@ -47,18 +44,39 @@ $header=array('GET /1575051 HTTP/1.1',
     );
 
     curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,0);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 300);
     curl_setopt( $ch, CURLOPT_COOKIESESSION, true );
+
+//    curl_setopt($ch, CURLOPT_USERPWD, "heigold1:heimer27");
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
 
     curl_setopt($ch,CURLOPT_COOKIEFILE,'cookies.txt');
     curl_setopt($ch,CURLOPT_COOKIEJAR,'cookies.txt');
     curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
-     return curl_exec($ch); 
-//    $result=curl_exec($ch);
-//    curl_close($ch);
+
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_STDERR,$f = fopen(__DIR__ . "/error.log", "w+"));
+
+
+    $returnHTML = curl_exec($ch); 
+
+if($errno = curl_errno($ch)) {
+    $error_message = curl_strerror($errno);
+    echo "cURL error ({$errno}):\n {$error_message}";
+}   
+   curl_close($ch);
+    return $returnHTML; 
+
+
+ 
 
 } // end of function grabHTML
+
+$ret = "";
+$finalReturn = "";
 
 if ($which_website == "marketwatch")
 {
@@ -73,103 +91,95 @@ $mwPartnerHeadlinesLink1Title = "";
 $mwPRHeadlinesLink1 = ""; 
 $mwPRHeadlinesLink1Title = ""; 
 
-      $url="http://$host_name/investing/$stockOrFund/$symbol/news";
+      $url="https://$host_name/investing/$stockOrFund/$symbol";
+
       $result = grabHTML($host_name, $url); 
-      $result = str_replace ('href="/', 'href="http://www.marketwatch.com/', $result);  
+
+      $result = str_replace ('href="/', 'href="https://www.marketwatch.com/', $result);  
       $result = str_replace ('heigoldinvestments.com', 'marketwatch.com', $result); 
       $result = str_replace ('localhost', 'www.marketwatch.com', $result); 
 
       $html = str_get_html($result);
 
-// 	  $positionText = strpos($html, "Object moved to"); 
-
       if (($pos = strpos($html, "<html><head><title>Object moved") > -1) && 
           ($stockOrFund == "stock"))
           {
-              $url="http://$host_name/investing/fund/$symbol/news";
+              $url="https://$host_name/investing/fund/$symbol";
               $result = grabHTML($host_name, $url); 
           }
       else if (($pos = strpos($html, "<html><head><title>Object moved") > -1) && 
           ($stockOrFund == "fund"))
           {
-              $url="http://$host_name/investing/stock/$symbol/news";
+              $url="https://$host_name/investing/stock/$symbol";
               $result = grabHTML($host_name, $url); 
           }
 
+      $result = str_replace ('href="/', 'href="https://www.marketwatch.com/', $result);  
+      $result = str_replace ('heigoldinvestments.com', 'marketwatch.com', $result); 
+      $result = str_replace ('localhost', 'www.marketwatch.com', $result); 
+      $result = preg_replace('/ etf/i', '<span style="background-color:red; color:black"><b> &nbsp;ETF</b>&nbsp;</span>', $result);
+      $result = preg_replace('/ etn/i', '<span style="background-color:red; color:black"><b> &nbsp;ETN</b>&nbsp;</span>', $result);
+      $result = str_replace ('a href', 'a onclick="return openPage(this.href)" href', $result);  
+
+
       $html = str_get_html($result);
- fwrite($file, "html results " . $html);
 
-      	// if the stock is found by marketwatch.  If the html string returned 
-		// contains "Object moved to", that means the symbol wasn't found.
-
-// fwrite($file, "html results for " . $symbol . " is " . $html);
 
         if (preg_match('/<title>Object moved<\/title>/i', $html))
         {
           $isFound = "notFound"; 
         }
         else 
-       	{
+        {
+          $isFound = "found"; 
 
-       		$isFound = "found";
+          $ret = $html->find('.j-tabPanes'); 
 
-      		$full_company_name = $html->find('#instrumentname'); 
-		    $entireMarketwatchPage = $html->find('#maincontent'); 
+          $firstNewsGroup = str_get_html($ret[0]);
+          $secondNewsGroup = str_get_html($ret[1]);
 
-		    // get the most recent link from the first main marketwatch headlines 
-		    // table 
+          $firstNewsGroupArticleContent = $firstNewsGroup->find('.article__content');
 
-		    $mainMWHeadlines = $html->find('#mwheadlines'); 
-			$linkTag = $mainMWHeadlines[0]->find('a');
-			$firstLinkInResults = $linkTag[0]; 
-			$mwMainContentLink1 = trim($firstLinkInResults->href); 
-			$mwMainContentLink1 = str_replace ('"', '\"', $mwMainContentLink1);  
-			$mwMainContentLink1Title = $firstLinkInResults->innertext;
-			$mwMainContentLink1Title = str_replace ('"', '\"', $mwMainContentLink1Title);  			
+          $firstNewsGroupFirstLinkHTML = str_get_html($firstNewsGroupArticleContent[0]);
 
-		    // get the most recent link from the first marketwatch PARTNER 
-		    // headlines table 
+          $firstNewsGroupLink = $firstNewsGroupFirstLinkHTML->find('a'); 
 
-		    $mwPartnerHeadlinesTable = $html->find('#partnerheadlines'); 
-			$linkTag = $mwPartnerHeadlinesTable[0]->find('a');
-			$firstLinkInResults = $linkTag[0]; 
-			$mwPartnerHeadlinesLink1 = trim($firstLinkInResults->href); 
-			$mwPartnerHeadlinesLink1 = str_replace ('"', '\"', $mwPartnerHeadlinesLink1);  			
-			$mwPartnerHeadlinesLink1Title = $firstLinkInResults->innertext;
-			$mwPartnerHeadlinesLink1Title = str_replace ('"', '\"', $mwPartnerHeadlinesLink1Title);  						
+          $mwMainContentLink1 = $firstNewsGroupLink[0]->href;
+          $mwMainContentLink1Title = $firstNewsGroupLink[0]->innertext;
 
-		    // get the most recent link from the first marketwatch 
-		    // PR headlines table 
+          // Other News
+          $secondNewsArray = array();
+          $secondNewsGroupArticleContent = $secondNewsGroup->find('.article__content');
 
-		    $mwPRHeadlinesTable = $html->find('#prheadlines'); 
-			$linkTag = $mwPRHeadlinesTable[0]->find('a');
-			$firstLinkInResults = $linkTag[0]; 
-			$mwPRHeadlinesLink1 = trim($firstLinkInResults->href); 
-			$mwPRHeadlinesLink1 = str_replace ('"', '\"', $mwPRHeadlinesLink1);				
-			$mwPRHeadlinesLink1Title = $firstLinkInResults->innertext;
-			$mwPRHeadlinesLink1Title = str_replace ('"', '\"', $mwPRHeadlinesLink1Title);  									
+          foreach ($secondNewsGroupArticleContent as $article)
+          {
+            $articleContent = str_get_html($article);
+            $dateTime = $articleContent->find('li.article__timestamp');
+            $dateTimeSpan = '<span style="font-size: 10px;">' . $dateTime[0]->text() . "</span>"; 
+            $headline = $articleContent->find('.article__headline');
+            $headline = preg_replace('/h3/', 'span', $headline); 
+            $timeStamp = preg_match('/data-est=\"(.*)\" class/', $dateTime[0], $timeStampMatches);
+            $timeInteger = strtotime($timeStampMatches[1]);
+            $secondNewsArray[$timeInteger] = $headline[0];
+          }
+          krsort($secondNewsArray);
 
-		} // if the stock is found by marketwatch
+          reset($secondNewsArray);
+          $first_key = key($secondNewsArray);
 
-		// return everything in the a final json object.
+          $secondNewsGroupFirstLinkHTML = str_get_html($secondNewsArray[$first_key]);
 
-/*		$returnArray = json_encode(array("found"=>$isFound, 
-										 "mwMainHeadlines"=>array("url"=>$mwMainContentLink1, "urlTitle"=>$mwMainContentLink1Title), 
-										 "mwPartnerHeadLines"=>array("url"=>$mwPartnerHeadlinesLink1, "urlTitle"=>$mwPartnerHeadlinesLink1Title), 
-										 "mwPRHeadLines"=>array("url"=>$mwPRHeadlinesLink1, "urlTitle"=>$mwPRHeadlinesLink1Title), 										 
-										),JSON_UNESCAPED_SLASHES); */
+          $secondNewsGroupLink = $secondNewsGroupFirstLinkHTML->find('a'); 
 
+          $mwPartnerHeadlinesLink1 = $secondNewsGroupLink[0]->href; 
+          $mwPartnerHeadlinesLink1Title = $secondNewsGroupLink[0]->innertext;
+        }    
 
-		// json_encode doesn't work on my server, so I'll just manually encode the json object.
+      $returnArray = '{"found":"' . $isFound . '",' . '"mwMainHeadlines":{"url":"' . $mwMainContentLink1 . '","urlTitle":"' . $mwMainContentLink1Title . '"},' . 
+            '"mwPartnerHeadLines":{"url":"' . $mwPartnerHeadlinesLink1 . '","urlTitle":"' . $mwPartnerHeadlinesLink1Title . '"},' . 
+            '"mwPRHeadLines":{"url":"' . $mwPRHeadlinesLink1 . '","urlTitle":"' . $mwPRHeadlinesLink1Title . '"}}'; 
 
-		$returnArray = '{"found":"' . $isFound . '",' . '"mwMainHeadlines":{"url":"' . $mwMainContentLink1 . '","urlTitle":"' . $mwMainContentLink1Title . '"},' . 
-						'"mwPartnerHeadLines":{"url":"' . $mwPartnerHeadlinesLink1 . '","urlTitle":"' . $mwPartnerHeadlinesLink1Title . '"},' . 
-						'"mwPRHeadLines":{"url":"' . $mwPRHeadlinesLink1 . '","urlTitle":"' . $mwPRHeadlinesLink1Title . '"}}'; 
-
-// 		fwrite($file, "right after json_encode.  array is " . $returnArray); 
-
-		echo $returnArray; 
-     
+    echo $returnArray; 
 }
 else if ($which_website == "yahoo")
 {
@@ -179,9 +189,6 @@ $ret = "";
 $url = "";
 $urlTitle = "";
 
-
-//      $url="http://$host_name/q/h?s=$symbol+Headlines"; 
-
       $url = "https://finance.yahoo.com/quote/$symbol?p=$symbol";
       $html = file_get_html($url);
 
@@ -189,7 +196,9 @@ $urlTitle = "";
 
       $company_name_array = $html->find('h6'); 
 
-      if (preg_match('/\"NOT_FOUND\"\:\"Not Found\"/i', $html))
+//echo "HTML IS " . $html; 
+
+      if (preg_match('/content\=\"symbol lookup/i', $html))
       {
           $isFound = "notFound"; 
       }
@@ -211,44 +220,31 @@ $urlTitle = "";
       $currentVolume = preg_replace('/<\/td>/', '', $currentVolume);
       $currentVolume = preg_replace('/<\/span>/', '', $currentVolume);   
 
-      	// if the stock is found by yahoo finance
+
+    // grab the news 
 
       $rss = simplexml_load_file("http://feeds.finance.yahoo.com/rss/2.0/headline?s=$symbol&region=US&lang=en-US");
 
-       	if (preg_match('/RSS feed not found/i', $rss->channel->item{0}->title))
+        if (preg_match('/RSS feed not found/i', $rss->channel->item{0}->title))
         {   // the symbol was not found by yahoo finance 
           $url = "";
           $urlTitle = "";
         }
         else
-       	{
-//      		$returnWithStrippedTags = preg_replace($patterns = array("/<img[^>]+\>/i", "/<embed.+?<\/embed>/im", "/<iframe.+?<\/iframe>/im", "/<script.+?<\/script>/im"), $replace = array("", "", "", ""), $ret[0]);
+        {
+//          $returnWithStrippedTags = preg_replace($patterns = array("/<img[^>]+\>/i", "/<embed.+?<\/embed>/im", "/<iframe.+?<\/iframe>/im", "/<script.+?<\/script>/im"), $replace = array("", "", "", ""), $ret[0]);
           $url = $rss->channel->item{0}->link;
-  				$urlTitle = $rss->channel->item{0}->title;
-    		} // if the symbol is found by yahoo finance
-
-		// now we retrieve the current volume
-
-   	
-
-/*		$returnArray = json_encode(array("found"=>$isFound, 
-										 "yahooInfo"=>array("urlTitle"=>$urlTitle, "url"=>$url)
-										 ), JSON_UNESCAPED_SLASHES); */ 
-
-//		$returnArray = '{"found":"' . $isFound . '",' . '"companyName":"' . $full_company_name[0] . '",' . '"yahooInfo":{"urlTitle":"' . $urlTitle . '","url":"' . $url . '"}}';
-
-		$returnArray = '{"found":"' . $isFound . '",' . '"companyName":"' . $full_company_name . '",' .  '"currentVolume":"' . $currentVolume  . '", "yahooInfo":{"urlTitle":"' . $urlTitle . '","url":"' . $url . '"}}';
-
-		echo $returnArray; 
-
-} // if $which_website = 'yahoo'
+          $urlTitle = $rss->channel->item{0}->title;
+        } // if the symbol is found by yahoo finance
 
 
 
+    $returnArray = '{"found":"' . $isFound . '",' . '"companyName":"' . $full_company_name . '",' .  '"currentVolume":"' . $currentVolume  . '", "yahooInfo":{"urlTitle":"' . $urlTitle . '","url":"' . $url . '"}}';
 
-//          fwrite($file, $tempFinalReturn);
-// 	echo(json_encode("test ")); 
+    echo $returnArray; 
+//        JSON string 
 
-// fclose( $file );
+} // if ($which_website == "yahoo")
+
 
 ?>
