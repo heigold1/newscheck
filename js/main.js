565b01d8de41eb68de7d1465ef6c89e3c7026fd8
Brent Heigold <brent@heigoldinvestments.com>
2024-06-27 22:42:11 -0700

    BigCharts

--- js/main.js
+++ js/main.js
@@ -1059,7 +1059,7 @@ function checkAllDivsForNews()
 								if ($("#checkForBigCharts" + currentId).is(':checked'))
 								{
    								$("#bigChartsPercentage" + currentId).html(bigChartsPercentage); 
-									if ((bigChartsDifference) < 9.75)							
+									if ((bigChartsDifference) < 10.00)							
 									{
 										$("#bigChartsWrapper" + currentId).css("background-color", "#FFA1A1");
 										globalBigChartsAlert = true;
