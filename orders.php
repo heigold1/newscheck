48560286f9abcab29ea9bb86293372ddde035413
Brent Heigold <brent@heigoldinvestments.com>
2024-12-17 05:58:34 -0800

    Widen the 'price changes' column

--- orders.php
+++ orders.php
@@ -22,7 +22,7 @@
     if ($result->num_rows > 0) {
 
 
-        $html .= "<tr><th>SYMBOL</th><th>ORDER STUB</th><th>VOLUME NOTES</th><th>NOTES</th></tr>"; 
+        $html .= "<tr><th>SYMBOL</th><th>ORDER STUB</th><th>ACTIONS</th><th>PRICE CHANGES</th></tr>"; 
         // output data of each row
         while($row = $result->fetch_assoc()) {
             $html .=  "<tr style='font-size: 11px;'>";
@@ -30,7 +30,7 @@
             $html .= "<td style='border: 1px solid black !important; font-size: 15px !important; font-family: arial; '>" . $row["symbol"] . "</td>";
             $html .= "<td style='border: 1px solid black !important; width: 300px; font-size: 15px !important; font-family: arial; '>" . $row["order_stub"] . "</td>"; 
             $html .= "<td style='border: 1px solid black !important; width: 300px; font-size: 15px  !important; font-family: arial; '>" . preg_replace("/-- /", "--<br>", $row["volume_notes"])  . "</td>"; 
-            $html .= "<td style='border: 1px solid black !important; width: 300px; font-size: 15px  !important; font-family: arial; '>" . preg_replace("/-- /", "--<br>", $row["individual_notes"]) . "</td>";
+            $html .= "<td style='border: 1px solid black !important; width: 400px; font-size: 15px  !important; font-family: arial; '>" . preg_replace("/-- /", "--<br>", $row["individual_notes"]) . "</td>";
             $html .= "</tr>";
         }
     } else {
